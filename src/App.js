daba5858811e09355e852b18993effd14471ae6f
mustaffaerdogan <mustafaerdoganu@gmail.com>
2023-08-04 11:03:01 +0300

    axios use effect added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-
+import axios from "axios";
 import './App.css';
+import { useEffect } from "react";
 
 function App() {
+  useEffect (() => {
+    async function getApi() {
+      try {
+        const response = await axios.get('/user?ID=12345');
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+    
+  )
   return (
     <div className="App">
       Hello Must
