f0d6bd0c483f1c1ab24e9582f9ca349ebb7852cd
mustaffaerdogan <mustafaerdoganu@gmail.com>
2023-08-05 21:13:20 +0300

    api open weather key add

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import './App.css';
 import { useEffect } from "react";
 
 function App() {
+  //api keyi sabite atadım 
+  const key = "1379df55a24681a58c21d4bc74bd92c6";
+
   useEffect (() => {
     async function getApi() {
       try {
-        const response = await axios.get('/user?ID=12345');
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=&appid=${key}`);
         console.log(response);
       } catch (error) {
         console.error(error);
       }
     }
+    getApi();
   }
+  ,[]
     
   )
   return (
